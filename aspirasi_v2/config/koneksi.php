9abe723d233886197e5943afd218c934839482f0
NabilNazuminn <nabilnazuminn@gmail.com>
2026-04-08 21:32:26 +0700

    Delete aspirasi_v2 directory

--- aspirasi_v2/config/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Koneksi database menggunakan MySQLi
-$koneksi = mysqli_connect("localhost", "root", "", "db_aspirasi");
-
-if (!$koneksi) {
-    die("Koneksi gagal: " . mysqli_connect_error());
-}
-?>
